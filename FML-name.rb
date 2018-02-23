b86993ad5420ece253872cc9534eb0bd1bd554a3
Micah Swarb <Swarbmicah@gmail.com>
2018-02-23 14:58:23 -0600

    test
    commit test

--- FML-name.rb
+++ FML-name.rb
@@ -5,3 +5,18 @@ mname = gets.chomp
 puts 'Ok, I\'ve got your middle name as ' + mname + '. Can I get your last name as well?'
 lname = gets.chomp
 puts 'Thank You!, your name is ' + ' ' + fname + ' ' + mname + ' ' + lname + '? What a great name!'
+
+puts '.........................'
+puts '.........................'
+puts '.........................'
+puts '.........................'
+puts '.........................'
+
+puts 'Tell me your name again? I forget... '
+name = fname + ' ' + mname + ' ' + lname
+puts '.........................'
+puts name + ' right?'
+puts '.........................'
+puts '.........................'
+puts '.........................'
+puts 'Did you know there are  ' + name.length.to_s + ' characters in your name?'
