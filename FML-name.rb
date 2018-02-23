20bb4bb2102e0c81123c53e9d78a601d278506ee
Micah Swarb <Swarbmicah@gmail.com>
2018-02-23 12:22:49 -0600

    Add files via upload

---
+++ FML-name.rb
@@ -0,0 +1,7 @@
+puts 'Hello there, can I get your first name please?'
+fname = gets.chomp
+puts 'Your first name is ' + fname + '? How about your middle name?'
+mname = gets.chomp
+puts 'Ok, I\'ve got your middle name as ' + mname + '. Can I get your last name as well?'
+lname = gets.chomp
+puts 'Thank You!, your name is ' + ' ' + fname + ' ' + mname + ' ' + lname + '? What a great name!'
