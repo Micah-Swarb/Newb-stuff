d755ebda37898206c72728a2d3252113e90fa22d
Micah Swarb <Swarbmicah@gmail.com>
2018-02-27 16:27:00 -0600

    englishNumber | Table of Contents
    created and finished table of contents using an array | started englishNumber

--- learn-to-program/toc.rb
+++ learn-to-program/toc.rb
@@ -1,8 +1,10 @@
-lineWidth = 100
+lineWidth = 85
 
-puts('Table of Contents'.center(lineWidth))
-puts('Chapter 1: Numbers'.ljust(lineWidth))
-puts('Chapter 2: Letters'.ljust(lineWidth))
-puts('Chapter 3: Variables'.ljust(lineWidth))
-puts('Chapter 4: Arrays'.ljust(lineWidth))
-puts('Chapter 5: Maths'.ljust(lineWidth))
+chapter = ['Table of Contents', 'Chapter 1: Micah', 'Chapter 2: Christi', 'Chapter 3: Micah + Christi', 'Chapter 4: Micah, Christi, and Nikolai', 'Chapter 5: .......']
+page = ['page 1', 'page 10', 'page 20', 'page 30', 'page 40']
+
+  puts(chapter[0].center(lineWidth/2) + page[0].rjust(lineWidth/2)) # this divides the line into two
+  puts(chapter[1].ljust(lineWidth/2) +  page[1].rjust(lineWidth/2))
+  puts(chapter[2].ljust(lineWidth/2) +  page[2].rjust(lineWidth/2))
+  puts(chapter[3].ljust(lineWidth/2) +  page[3].rjust(lineWidth/2))
+  puts(chapter[4].ljust(lineWidth/2) +  page[4].rjust(lineWidth/2))
