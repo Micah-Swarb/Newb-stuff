d0cc4f874ba703baa676685b4056b0f716dd9d5e
Micah Swarb <Swarbmicah@gmail.com>
2018-03-02 16:19:14 -0600

    Begin codecademy Ruby course

---
+++ codecademy-learn_ruby/formatting--with-string-methods.rb
@@ -0,0 +1,33 @@
+print 'whats your first name?'
+first_name = gets.chomp
+first_name.capitalize! #notice the ! at the end of capitalize.
+                       #This modifies the value contained within the variable answer itself.
+
+puts "your name is #{first_name}!"
+
+print 'whats your last name?'
+last_name = gets.chomp
+last_name.capitalize!
+
+puts "your last name is #{last_name}!"
+
+print 'whats city were you born?'
+city = gets.chomp
+city.capitalize!
+
+puts "your city is #{city}"
+
+print 'whats state?'
+state = gets.chomp
+state.upcase!
+
+puts "your state is #{state}!"
+
+
+=begin
+If you define a variable monkey that's equal
+to the string "Curious George", and then you have a string that says
+"I took #{monkey} to the zoo", Ruby will do something called
+string interpolation and replace the #{monkey} bit with the value
+of monkey—that is, it will print "I took Curious George to the zoo"
+=end
--- learn-to-program/name-count.rb
+++ learn-to-program/name-count.rb
@@ -1,13 +1,20 @@
 puts 'Would you like to know the number of characters in yor name?'
 puts 'I\'m going to assume you said yes. So whats your first name?'
 fname = gets.chomp
+fname.capitalize!
+
 puts 'Your first name is ' + fname + '? How about your middle name?'
 mname = gets.chomp
+mname.capitalize!
+
 puts 'Ok, I\'ve got your middle name as ' + mname + '. Can I get your last name as well?'
 lname = gets.chomp
+lname.capitalize!
+
 puts 'ok, great! '
 puts 'Did you know there are  ' + fname.length.to_s + ' characters in your first name?'
 puts 'and ' + mname.length.to_s + ' characters in your middle name.'
 puts 'annnnd your last name has ' + lname.length.to_s + ' characters'
-puts 'that brings us up to a to total of: '
+
+puts 'that brings our character count to a total of: '
 puts fname.length.to_i + mname.length.to_i + lname.length.to_i
